4900ff1806ba1a31d1f51b6eaf95c2b1f3c5c2db
MalihaChaudhry <139657103+MalihaChaudhry@users.noreply.github.com>
2024-07-26 09:03:45 -0500

    final fixes of header and footer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,12 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header className='header-container'>
-      <h1 className='header'>
-        <span className='header-happy'>Happy</span>{' '}
-        <span className='header-56'>56th</span>{' '}
-        <span className='header-birthday'>Birthday</span>{' '}
-        <span className='header-amee'>Amee!</span>
-      </h1>
+      <h1 className='header'>Happy 56th Birthday Amee!</h1>
     </header>
   );
 };
