c0b0f29a1aca0a1855ab69ea9675354bab5c8312
MalihaChaudhry <139657103+MalihaChaudhry@users.noreply.github.com>
2024-07-26 00:22:11 -0500

    Deployment updates

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/happy-birthday-amee',
   plugins: [react()],
   define: {
     global: {},
   },
-  assetsInclude: ['**/*.MOV'],
 });
